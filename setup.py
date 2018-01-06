8cb6b8de82d0a71367a7bc2eddd8ce2d566c7d3d
yoogx <hugues.jerome@gmail.com>
2018-01-06 18:42:40 +0100

    * Better setup file

--- setup.py
+++ setup.py
@@ -6,12 +6,23 @@ def readme():
 
 setup(name             = 'fmu2aadl',
       version          = '0.1',
-      description      = 'FMU2AADL converter',
+      url = 'https://github.com/OpenAADL/fmu2aadl',
+      description      = 'FMU-to-AADL converter',
       long_description = readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: Eclipse Public License 1.0 (EPL-1.0)',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Code Generators',
+      ],
+      license = "EPL-1.0",
       packages         = ['fmu2aadl'],
       entry_points     = {
           'console_scripts' : ['fmu2aadl=fmu2aadl.command_line:main'],
       },
+      install_requires=[
+          'docopt', 'lxml',
+      ],
       include_package_data = True,
       zip_safe = False
 )
