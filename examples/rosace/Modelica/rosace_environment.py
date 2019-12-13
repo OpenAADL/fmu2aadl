1f21d9241b5d995ac8b432567e63882396cf9700
hugues <hugues@TwinOps>
2019-12-13 10:41:10 -0500

    * Update to JModelica 2.14

--- examples/rosace/Modelica/rosace_environment.py
+++ examples/rosace/Modelica/rosace_environment.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from pymodelica import compile_fmu
 from pyfmi import load_fmu
 
@@ -6,3 +8,6 @@ def run_demo():
 	engine_fmu = compile_fmu("Engine","Engine.mo", target='cs', version='2.0');
 	elevator_fmu = compile_fmu("Elevator","Elevator.mo", target='cs', version='2.0')
 	aircraft_dynamics_fmu = compile_fmu("Aircraft_Dynamics","Aircraft_Dynamics.mo", target='cs', version='2.0')
+
+if __name__ == "__main__":
+        run_demo()
